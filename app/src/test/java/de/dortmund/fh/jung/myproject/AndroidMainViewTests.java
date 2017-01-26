3ba876ebb3accfa7aa719a3583932962faf7e855
Jan-Hendrik Jung <jung@adesso-mobile.de>
2017-01-26 11:14:39 +0100

    RobolectricTestingSupport: added Robolectric and some simple ExampleTests

---
+++ app/src/test/java/de/dortmund/fh/jung/myproject/AndroidMainViewTests.java
@@ -0,0 +1,37 @@
+package de.dortmund.fh.jung.myproject;
+
+
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Button;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import de.dortmund.fh.jung.myproject.mainview.MainActivity;
+import de.dortmund.fh.jung.myproject.searchview.SearchActivity;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 22)
+public class AndroidMainViewTests{
+
+    @Test
+    public void firstTest(){
+        MainActivity activity = Robolectric.setupActivity(MainActivity.class);
+        FloatingActionButton button = (FloatingActionButton)activity.findViewById(R.id.fab);
+        assertEquals(button.getVisibility(), View.VISIBLE);
+    }
+
+    @Test
+    public void searchViewButtonTextTest() {
+        SearchActivity activity = Robolectric.setupActivity(SearchActivity.class);
+        Button button = (Button)activity.findViewById(R.id.search_button);
+        assertEquals(button.getText(),"search");
+    }
+
+}
